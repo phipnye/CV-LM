bdca663feba9c657ed4c9c743105429105c206c5
phipnye <phipnye@proton.me>
2026-01-05 13:05:07 -0800

    Add explicit build standard for structured bindings support and suppress Eigen attribute warnings

--- src/CV-WorkerModel.h
+++ src/CV-WorkerModel.h
@@ -8,6 +8,11 @@ namespace OLS {
 
 struct WorkerModel {
   WorkerModel() = default;
+  WorkerModel(const WorkerModel&) = default;
+  WorkerModel(WorkerModel&&) = default;
+  WorkerModel& operator=(const WorkerModel&) = delete;
+  WorkerModel& operator=(WorkerModel&&) = delete;
+  
   void computeBeta(const Eigen::Ref<const Eigen::MatrixXd>& xTrain,
                    const Eigen::Ref<const Eigen::VectorXd>& yTrain,
                    Eigen::VectorXd& beta) const;
@@ -19,10 +24,13 @@ namespace Ridge {
 
 struct WorkerModel {
   const double lambda_;
-  mutable Eigen::MatrixXd xtxLambda_;  // mutable allows reuse in const methods
+  mutable Eigen::MatrixXd xtxLambda_;  // mutable allows reuse in beta computation
 
   explicit WorkerModel(const double lambda, const Eigen::Index ncol);
   explicit WorkerModel(const WorkerModel& other);
+  WorkerModel(WorkerModel&&) = default;
+  WorkerModel& operator=(const WorkerModel&) = delete;
+  WorkerModel& operator=(WorkerModel&&) = delete;
 
   void computeBeta(const Eigen::Ref<const Eigen::MatrixXd>& xTrain,
                    const Eigen::Ref<const Eigen::VectorXd>& yTrain,
